b9101f933c9192905a82d5c04fe71444f13e5e1b
DanielKyjas <dani.kyjas@hotmail.com>
2024-01-12 12:38:50 -0600

    intentosd emodificar el carrusel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 
   <template>
+    <div class="items5">
     <MDBCarousel
       v-model="carousel5"
       :items="items5"
+      inner-class=" w-100 "
+      captionsClass="carousel-caption w-100"
+      items-class="w-100 "
+      
+      dark
     />
+  </div>
   </template>
 
 
@@ -13,25 +20,37 @@
 
   const items5 = [
     {
-      src: "https://mdbootstrap.com/img/Photos/Slides/img%20(15).webp",
+   
+      src: "src/assets/fachada dia con logo.jpg",
       alt: "...",
       label: "First slide label",
-      caption: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+      caption: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+     
+     
     },
     {
-      src: "https://mdbootstrap.com/img/Photos/Slides/img%20(22).webp",
+      src: "src/assets/TERRAZA ASADOR. LOGO.jpg",
       alt: "...",
       label: "Second slide label",
       caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
-      src: "https://mdbootstrap.com/img/Photos/Slides/img%20(23).webp",
+      src: "src/assets/VISTA MURO INTERIOR.jpg",
       alt: "...",
       label: "Third slide label",
       caption:
-        "Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+        "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+        captionClass: "red"
     }
   ];
 
   const carousel5 = ref(0);
 </script>
+<style>
+  .items5{
+    width: 100%;
+    height: 85dvh;
+  
+  }
+
+</style>
