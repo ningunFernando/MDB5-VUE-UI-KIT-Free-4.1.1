e9654ccc1e74d0c3b7c5f8b8b7be14c2ce11834f
DanielKyjas <dani.kyjas@hotmail.com>
2024-01-12 16:19:27 -0600

    cambios locos
    funcionan en local host

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,49 +1,58 @@
 <template>
     <div class="items5">
-      <MDBCarousel
-        v-model="carousel5"
-        :items="items5"
-        inner-class="w-100 d-flex justify-content-start "
-        captionsClass="carousel-caption h-50 "
-        items-class="h-100"
-      />
-    </div>
+    <MDBCarousel
+
+      v-model="carousel5"
+      :items="items5"
+      inner-class="w-100 d-flex justify-content-start height-75"
+      captionsClass="carousel-caption h-50 "
+      items-class="h-100 w-100 h-xs-100"
+
+      
+    />
+  </div>
   </template>
-  
-  <script setup lang="ts">
-  import { ref, onMounted, onBeforeUnmount } from "vue";
+
+
+<script setup lang="ts">
+  import { ref } from "vue";
   import { MDBCarousel } from "mdb-vue-ui-kit";
+  let responsive = screen.width;
+  let image;
+  if (responsive > 750)
+  {
+    image = true;
+  }else{
+    image = false;
+  }
+
+
   
   const items5 = [
     {
-      src: window.innerWidth > 750 ? "src/assets/images/sa_1 - Photo.jpg" : "src/assets/images/sav_1 - Photo.jpg",
+      src: image?"src/assets/sa_1.jpg":"src/assets/sav_1.jpg",
       alt: "...",
       label: "First slide label",
       caption: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
-    },
+      },
     {
-      src: window.innerWidth > 750 ? "src/assets/images/sa_2 - Photo.jpg" : "src/assets/images/sav_2 - Photo.jpg",
+      src: image?  "src/assets/sa_2.jpg":"src/assets/sav_2.jpg",
       label: "Second slide label",
-      caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
-      src: window.innerWidth > 750 ? "src/assets/images/sa_3 - Photo.jpg" : "src/assets/images/sav_4 - Photo.jpg",
-      alt: "...",
+      src:image?"src/assets/sa_4.jpg":"src/assets/sav_4.jpg",
+      alt: "hola",
       label: "Third slide label",
-      caption: "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
-      captionClass: "red",
-    },
+      caption:
+        "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+    }
   ];
-  
-  const carousel5 = ref(0);
-  
-  
 
-  </script>
+  const carousel5 = ref(0);
+</script>
+<style>
+
+
   
-  <style>
-  .items5 {
-   
-  }
-  </style>
-  
+</style>
