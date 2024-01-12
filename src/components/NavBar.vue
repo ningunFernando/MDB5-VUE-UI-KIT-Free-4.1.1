17589760be2bbf99045deefe4dc8ccd0bad10720
ningunFernando <117299803+ningunFernando@users.noreply.github.com>
2024-01-12 11:23:54 -0600

    NAV MODIFICADA

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,40 +13,8 @@
           <MDBNavbarItem href="#">
             Link
           </MDBNavbarItem>
-          <MDBNavbarItem>
-            <!-- Navbar dropdown -->
-            <MDBDropdown class="nav-item" v-model="dropdown1">
-              <MDBDropdownToggle
-                tag="a"
-                class="nav-link"
-                @click="dropdown1 = !dropdown1"
-                >Dropdown</MDBDropdownToggle
-              >
-              <MDBDropdownMenu aria-labelledby="dropdownMenuButton">
-                <MDBDropdownItem href="#">Action</MDBDropdownItem>
-                <MDBDropdownItem href="#">Another Action</MDBDropdownItem>
-                <MDBDropdownItem href="#"
-                  >Something else here</MDBDropdownItem
-                >
-              </MDBDropdownMenu>
-            </MDBDropdown>
-          </MDBNavbarItem>
-          <MDBNavbarItem to="#" disabled>
-            Disabled
-          </MDBNavbarItem>
         </MDBNavbarNav>
         <!-- Search form -->
-        <form class="d-flex input-group w-auto">
-          <input
-            type="search"
-            class="form-control"
-            placeholder="Type query"
-            aria-label="Search"
-          />
-          <MDBBtn outline="primary">
-            Search
-          </MDBBtn>
-        </form>
       </MDBCollapse>
     </MDBNavbar>
   </template>
