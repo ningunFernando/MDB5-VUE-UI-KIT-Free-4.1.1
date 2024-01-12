41ce5727ddc25b6ff79079839364df921b57a9a6
ningunFernando <117299803+ningunFernando@users.noreply.github.com>
2024-01-12 11:12:48 -0600

    TEST

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import NavBar from './components/NavBar.vue';
+</script>
+
+<template>
+  <NavBar/>
+  <router-view />
+</template>
+
+<style scoped>
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+
+  <template>
+    <MDBCarousel
+      v-model="carousel5"
+      :items="items5"
+    />
+  </template>
+
+
+<script setup lang="ts">
+  import { ref } from "vue";
+  import { MDBCarousel } from "mdb-vue-ui-kit";
+
+  const items5 = [
+    {
+      src: "https://mdbootstrap.com/img/Photos/Slides/img%20(15).webp",
+      alt: "...",
+      label: "First slide label",
+      caption: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+    {
+      src: "https://mdbootstrap.com/img/Photos/Slides/img%20(22).webp",
+      alt: "...",
+      label: "Second slide label",
+      caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      src: "https://mdbootstrap.com/img/Photos/Slides/img%20(23).webp",
+      alt: "...",
+      label: "Third slide label",
+      caption:
+        "Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+    }
+  ];
+
+  const carousel5 = ref(0);
+</script>
