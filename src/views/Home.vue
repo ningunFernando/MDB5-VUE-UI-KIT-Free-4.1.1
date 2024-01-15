54dd11db0c7cc16114748069c44305ef8cdd606a
DanielKyjas <dani.kyjas@hotmail.com>
2024-01-15 11:56:03 -0600

    carrusel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,7 @@
   import WelcomeView from '../components/WelcomeView.vue';
   import UtilitiesView from '../components/UtilitiesView.vue';
   import ModelsTab from '../components/ModelsTab.vue';
+  
 </script>
 
 <style scoped>
